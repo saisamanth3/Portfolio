815a48e8c9434b22911b70f599b371302f1955f3
Sai CH <sai.ch@emindcomputing.com>
2019-08-02 13:14:22 +0530

    Build added
    Build added

---
+++ precache-manifest.e7f9f1e578b4e9c014125e8cd94de429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ecb7d204b7d1fea0b91212beef4815",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "225f847eb8859d935ff1",
+    "url": "/Portfolio/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "225f847eb8859d935ff1",
+    "url": "/Portfolio/static/js/main.55db051e.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Portfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);
